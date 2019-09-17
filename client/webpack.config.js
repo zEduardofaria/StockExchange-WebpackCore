2bf2f320fbce7d49d393564269955c2c6b214e85
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-17 19:47:54 -0300

    Adding good practices like scope hoisting, code splitting, lazy loading, api global address

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,17 +3,44 @@ const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
+
+plugins.push(new HtmlWebpackPlugin({
+
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        remvoeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+
+}));
 
 plugins.push(new extractTextPlugin('styles.css'))
 
 plugins.push(new webpack.ProvidePlugin({
     '$': 'jquery/dist/jquery.js',
     'jQuery': 'jquery/dist/jquery.js'
+}));
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+
 }))
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if (process.env.NODE_ENV == 'production') {
+
+    SERVICE_URL = JSON.stringify('http://endereco-da-sua-api');
+
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
 
     plugins.push(new babiliPlugin());
 
@@ -25,15 +52,21 @@ if (process.env.NODE_ENV == 'production') {
             }
         },
         canPrint: true
-    }))
+    }));
 }
 
+plugins.push(new webpack.DefinePlugin({
+    SERVICE_URL
+}));
+
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
